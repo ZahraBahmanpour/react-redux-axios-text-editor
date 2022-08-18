0d11e9fbb737dbd932a87dbdbcbb31cddfeed88f
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-08-18 15:09:31 +0430

    add user slice and api

---
+++ src/redux/features/user/usersSlice.js
@@ -0,0 +1,28 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { loginRequest } from "../../../api/users";
+const initialState = {
+  error: "",
+  isLoggedIn: false,
+};
+
+export const login = createAsyncThunk("users/login", (user) =>
+  loginRequest(user)
+);
+
+export const usersSlice = createSlice({
+  name: "users",
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(login.fulfilled, (state, action) => {
+      return { ...state, isLoggedIn: true };
+    });
+    builder.addCase(login.rejected, (state, action) => {
+      return {
+        error: action.error.message,
+        isLoggedIn: false,
+      };
+    });
+  },
+});
+
+export default usersSlice.reducer;
