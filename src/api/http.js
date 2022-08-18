f3566260989c3cda1818600c5c9fe210d6d3871a
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-08-18 11:40:28 +0430

    add axios.create

--- src/api/http.js
+++ src/api/http.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { BASE_URL } from "../config/api";
 
-export default axios.create({
-  baseURL: BASE_URL,
-});
+// export default axios.create({
+//   baseURL: BASE_URL,
+// });
+axios.defaults.baseURL = BASE_URL;
+export default axios;
