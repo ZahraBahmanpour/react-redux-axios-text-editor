f1895ee52983337962ac042d66c7245830356e33
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-08-18 11:59:28 +0430

    add login page and route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Container } from "react-bootstrap";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Tabs from "./components/tabs/Tabs";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import "./index.css";
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
           <Route path="/" element={<Home />}>
             <Route path=":id" element={<Tabs />} />
           </Route>
+          <Route path="login" element={<Login />} />
           <Route path="*" element={<div>Not Found</div>} />
         </Routes>
       </Container>
