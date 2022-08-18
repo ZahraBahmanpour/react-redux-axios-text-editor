eb917789e1a22afdcaa16a4b53c34bc62359578a
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-08-18 17:39:46 +0430

    add IS_LOGGED_IN constant

--- src/config/constants.js
+++ src/config/constants.js
@@ -3,3 +3,4 @@ export const CRUD_MODE_CREATE = "CRUD_MODE_CREATE";
 
 export const ACCESS_TOKEN = "ACCESS_TOKEN";
 export const REFRESH_TOKEN = "REFRESH_TOKEN";
+export const IS_LOGGGED_IN = "IS_LOGGGED_IN";
