ad99a946547fbad1667887284f67f1bdcb1cb195
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-08-18 11:41:40 +0430

    fix post requests

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,10 +1,21 @@
-import { POSTS } from "../config/api";
+import { POSTS_URL } from "../config/api";
 import axios from "./http";
 
 export const fetchAllPostsRequest = async () => {
   try {
-    const response = await axios.get(`${POSTS}`);
+    const response = await axios.get(POSTS_URL);
     return response.data;
+    // Promise.all
+    // const response = await Promise.all(
+    //   [POSTS, "/cities"].map((endpoint) => axios.get(endpoint))
+    // );
+    // const [{ data: posts }, { data: cities }] = response;
+    //Abort Controller
+    // const controller = new AbortController();
+    // const response = await axios.get(`${POSTS}`, {
+    //   signal: controller.signal,
+    // });
+    // return response.data;
   } catch (error) {
     return Promise.reject(error);
   }
@@ -12,7 +23,7 @@ export const fetchAllPostsRequest = async () => {
 
 export const updatePostRequest = async (post) => {
   try {
-    const response = await axios.put(`${POSTS}/${post.id}`, post);
+    const response = await axios.put(`${POSTS_URL}/${post.id}`, post);
     return response.data;
   } catch (error) {
     return Promise.reject(error);
@@ -21,7 +32,7 @@ export const updatePostRequest = async (post) => {
 
 export const createPostRequest = async (post) => {
   try {
-    const response = await axios.post(`${POSTS}`, post);
+    const response = await axios.post(`${POSTS_URL}`, post);
     return response.data;
   } catch (error) {
     return Promise.reject(error);
@@ -30,7 +41,7 @@ export const createPostRequest = async (post) => {
 
 export const deletePostRequest = async (post) => {
   try {
-    const response = await axios.delete(`${POSTS}/${post.id}`);
+    const response = await axios.delete(`${POSTS_URL}/${post.id}`);
     return response.data;
   } catch (error) {
     return Promise.reject(error);
