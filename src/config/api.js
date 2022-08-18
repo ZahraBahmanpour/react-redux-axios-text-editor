91e59258cfe89396bfbf7eebcb525d422c1ecb6d
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-08-18 15:05:10 +0430

    add login and refresh token apis

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,8 @@
-export const BASE_URL = "https://62a4d0d547e6e4006398b48b.mockapi.io/api";
+export const BASE_URL = "http://localhost:3002";
 
 // post
 export const POSTS_URL = "/posts";
+
+// users
+export const LOGIN_URL = "/auth/login";
+export const REFRESH_TOKEN_URL = "/auth/refresh-token";
