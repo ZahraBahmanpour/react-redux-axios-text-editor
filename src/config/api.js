0e305c462a721521df9aa535d9fd0271a64b5239
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-08-23 15:18:18 +0430

    changed base_url

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:3002";
+// export const BASE_URL = "http://localhost:3002";
+export const BASE_URL = "https://6300a18859a8760a757d441c.mockapi.io";
 
 // post
 export const POSTS_URL = "/posts";
