c8e92994546f2addcb0253f1e7dec62573819377
Zahra Bahmanpour <z.bmnpr@gmail.com>
2023-01-20 19:40:01 +0330

    remove private route

--- src/App.js
+++ src/App.js
@@ -4,21 +4,13 @@ import Tabs from "./components/tabs/Tabs";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import "./index.css";
-import { PrivateRoute } from "./route/PrivateRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Container className="mt-2">
         <Routes>
-          <Route
-            path="/"
-            element={
-              <PrivateRoute>
-                <Home />
-              </PrivateRoute>
-            }
-          >
+          <Route path="/" element={<Home />}>
             <Route path=":id" element={<Tabs />} />
           </Route>
           <Route path="login" element={<Login />} />
